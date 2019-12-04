0fa1aa3ca14d18fd4d35bba91061f935244e0aca
leandrofahur <fahurleandro@gmail.com>
2019-12-04 10:45:09 -0200

    process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,44 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+Process::Process(int pid) : pid_(pid) {
+    this->user_ = LinuxParser::User(this->pid_);
+    this->command_ = LinuxParser::Command(this->pid_);
+}
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+int Process::Pid() { return this->pid_; }
 
-// TODO: Return the command that generated this process
+float Process::CpuUtilization() {
+    long jiffieStart, jiffieEnd, uptimeStart, upTimeEnd;
+    
+    jiffieStart = LinuxParser::Jiffies();
+    uptimeStart = LinuxParser::UpTime(this->pid_);
+
+    unsigned int microsseconds;
+    usleep(10000);
+
+    jiffieEnd = LinuxParser::Jiffies();
+    upTimeEnd = LinuxParser::UpTime(this->pid_);
+
+    return (jiffieEnd - jiffieStart)/(upTimeEnd - uptimeStart);
+}
+
 string Process::Command() { return string(); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return LinuxParser::Ram(this->pid_); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { return this->user_; }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { return LinuxParser::UpTime(this->pid_); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const { 
+    long myRam = std::stol(LinuxParser::Ram(this->pid_));
+    long otherRam = std::stol(LinuxParser::Ram(a.pid_));
+    return myRam > otherRam; 
+}
