1730a3df985750e1328fd147584abd6878aac25b
leandrofahur <fahurleandro@gmail.com>
2019-12-06 20:14:26 -0200

    Operating system and Kernel

--- src/format.cpp
+++ src/format.cpp
@@ -2,10 +2,10 @@
 
 #include "format.h"
 
-using std::string;
+// using std::string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+std::string Format::ElapsedTime(long seconds[[maybe_unused]]) { return std::string(); }
